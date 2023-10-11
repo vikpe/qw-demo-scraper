1a31c4ce1b4a038c522a237d101d2759a98f0f85
Viktor Persson <viktor.persson@arcsin.se>
2023-10-11 18:06:55 +0200

    Add function to download files using requests.

---
+++ util.py
@@ -0,0 +1,10 @@
+import requests
+
+
+def download_file(url: str, dest: str) -> str:
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(dest, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return dest
