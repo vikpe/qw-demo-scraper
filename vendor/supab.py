1944cfd35822a2708db77f766b6f34e8ddb20eff
Viktor Persson <viktor.persson@arcsin.se>
2023-10-12 14:20:57 +0200

    Refactor supabase.

--- vendor/supab.py
+++ vendor/supab.py
@@ -1,5 +1,5 @@
 import os
-from typing import List, Optional
+from typing import Optional, List
 
 import attr
 from supabase import create_client, Client
@@ -7,7 +7,6 @@ from supabase import create_client, Client
 
 @attr.define
 class Demo:
-    id: Optional[int] = attr.ib()
     sha256: str = attr.ib()
     source: str = attr.ib()
     filename: str = attr.ib()
@@ -17,17 +16,12 @@ class Demo:
     mode: str = attr.ib()
     map: str = attr.ib()
     title: str = attr.ib()
-    created_at: Optional[str] = attr.ib()
+    players: List[dict] = attr.ib()
+    id: Optional[int] = attr.ib(default=0)
+    created_at: Optional[str] = attr.ib(default=None)
 
 
 def get_client() -> Client:
     url: str = os.environ.get("SUPABASE_URL")
     key: str = os.environ.get("SUPABASE_KEY")
     return create_client(url, key)
-
-
-def get_demos_source_filenames() -> List[str]:
-    sb: Client = get_client()
-    query = sb.table("demos").select("filename").execute()
-
-    return [Demo(**demo).filename for demo in query.data]
