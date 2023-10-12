ea52d71d7bc29d85bc22199eb00cf6e83d04f114
Viktor Persson <viktor.persson@arcsin.se>
2023-10-12 14:21:04 +0200

    Refactor aws.

--- vendor/aws.py
+++ vendor/aws.py
@@ -1,19 +1,12 @@
 import boto3
 
-from vendor import hub
 
+def upload(filepath: str, key: str, metadata=None):
+    if metadata is None:
+        metadata = {}
 
-def upload_recent_demo(s3: boto3.client, demo: hub.Demo, sha256: str):
     bucket = "quakeworld"
-    filename = f"{demo.filename}.gz"
-    filepath = f"demos/{filename}"
-    key = f"qw/demos/recent/{filename}"
-    metadata = {
-        "sha256": sha256,
-        "qtv_address": demo.qtv_address,
-        "filename": demo.filename,
-    }
-
+    s3 = boto3.client("s3")
     s3.upload_file(
         filepath,
         bucket,
